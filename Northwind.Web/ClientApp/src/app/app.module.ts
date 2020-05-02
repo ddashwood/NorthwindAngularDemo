13838a987207beab21038e21938cbb2fb9112238
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2020-05-02 21:34:55 +0100

    Re-generate entity framework classes with singular names

--- Northwind.Web/ClientApp/src/app/app.module.ts
+++ Northwind.Web/ClientApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { NorthwindModule } from './northwind/northwind.module'
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ProductListComponent } from './northwind/product-list/product-list.component';
 
 @NgModule({
   declarations: [
@@ -22,10 +24,12 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    NorthwindModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'products', component: ProductListComponent }
     ])
   ],
   providers: [],
