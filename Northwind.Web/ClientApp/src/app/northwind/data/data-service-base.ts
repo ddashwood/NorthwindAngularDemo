193223962afe3bc09931517c6bb498e91d8dc388
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2020-05-06 15:19:39 +0100

    Re-factoring

---
+++ Northwind.Web/ClientApp/src/app/northwind/data/data-service-base.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from "@angular/common/http";
+import { PagedResults } from "./paged-results";
+
+export class DataServiceBase<T> {
+  constructor(private http: HttpClient, private baseUrl: string, private apiController: string) {
+  }
+
+  public loadAll(page: number, pageSize: number, complete: (result: PagedResults<T>) => void): void {
+    let url = this.baseUrl + `api/${this.apiController}?${encodeURIComponent('page')}=${encodeURIComponent(page)}` +
+      `&${encodeURIComponent('pageSize')}=${encodeURIComponent(pageSize)}`;
+
+    this.http.get<PagedResults<T>>(url).subscribe(result => {
+      complete(result);
+    }, error => console.error(error));
+  }
+
+  public loadSingle(id: number, complete: (item: T) => void): void {
+    let url = this.baseUrl + `api/${this.apiController}/${encodeURIComponent(id)}`
+
+    this.http.get<T>(url).subscribe(result => {
+      complete(result);
+    }, error => console.error(error));
+  }
+}
--- Northwind.Web/ClientApp/src/app/northwind/data/products-data.service.ts
+++ Northwind.Web/ClientApp/src/app/northwind/data/products-data.service.ts
@@ -1,30 +1,13 @@
 import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { PagedResults } from './PagedResults';
+import { DataServiceBase } from './data-service-base';
 
 @Injectable()
-export class ProductsDataService {
-  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
-  }
-
-  public loadAll(page: number, pageSize: number, complete: (result: PagedResults<Product>) => void): void {
-    let url = this.baseUrl + `api/product?${encodeURIComponent('page')}=${encodeURIComponent(page)}` +
-      `&${encodeURIComponent('pageSize')}=${encodeURIComponent(pageSize)}`;
-
-    this.http.get<PagedResults<Product>>(url).subscribe(result => {
-      complete(result);
-    }, error => console.error(error));
-  }
-
-  public loadSingle(id: number, complete: (product: Product) => void): void {
-    let url = this.baseUrl + `api/product/${encodeURIComponent(id)}`
-
-    this.http.get<Product>(url).subscribe(result => {
-      complete(result);
-    }, error => console.error(error));
+export class ProductsDataService extends DataServiceBase<Product> {
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    super(http, baseUrl, "product")
   }
 }
-
 
 export interface Product {
   id: number;
